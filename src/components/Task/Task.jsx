cdd1db693df0b04df727f43805a4865ad64d7445
Elena <ed41658@gmail.com>
2024-12-15 12:58:18 +0500

    add modificators done and favorite

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "./task.module.scss";
 import cn from 'classnames';
 import Button from '../Button/Button';
 
 const Task = ({id, title, category, todo}) => {
+  const [isDone, setIsDone] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+  
   return (
-    <div className={cn(styles['task'])}>
+    <div className={cn(styles['task'], isDone && styles['task--done'], isFavorite ? styles['task--favorite'] : "")}>
 			<h3 className={cn(styles['task__title'])}>{title}</h3>
 			<span className={cn(styles['task__category'])}>{category}</span>
             <p className={cn(styles['task__desc'])}>{todo}</p>
             <div className={cn(styles['task__control'])}>
                 <Button use='remove' text='remove' handler={() => {}}/>
-                <Button use='favorite' text='favorite' handler={() => {}}/>
-                <Button use='done' text='done' handler={() => {}}/>
+                <Button use='favorite' text='favorite' handler={() => setIsFavorite(!isFavorite)}/>
+                <Button use='done' text='done' handler={() => setIsDone(!isDone)}/>
             </div>
 		</div>
   )
